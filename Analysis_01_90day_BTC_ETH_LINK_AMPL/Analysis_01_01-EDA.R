6de6a77e353865aed8735e870847bb267e070b98
Carlos R. Mercado <35497842+CR-Mercado@users.noreply.github.com>
2020-08-30 12:44:26 -0400

    correlation plot
    General Correlations done

--- Analysis_01_90day_BTC_ETH_LINK_AMPL/Analysis_01_01-EDA.R
+++ Analysis_01_90day_BTC_ETH_LINK_AMPL/Analysis_01_01-EDA.R
@@ -1,6 +1,7 @@
 # Analysis 1: Comparing BTC, ETC, ChainLink, and AMPL over the past 90 days. 
 
 library(lubridate)
+library(ggplot2)
 
 # Step 01 Data Load ---- 
 
@@ -13,7 +14,9 @@ chainlink <- read.csv(file = "Data/chainlink_2020-06-01_2020-08-30.csv",
 ethereum <- read.csv(file = "Data/ethereum_2020-06-01_2020-08-30.csv",
                      stringsAsFactors = FALSE)
 
-# Step 02 Cleanup and Feature Selection ---- 
+# Step 02 Cleanup ---- 
+#' Mostly just looking around for weird patterns. 
+
  # combine into a list for easier manipulation en masse. 
 my_coins <- list("ampleforth" = ampleforth,
                  "bitcoin" = bitcoin,
@@ -25,8 +28,8 @@ lapply(my_coins, function(x){
 nrow(x)
 }) 
 
-# fix their classes (I use CSV for easier portability, I could have used .rds to
-# preserve the Time as a class POSIXct). 
+# fix their classes (I used CSV for easier portability, 
+# you can use .rds to preserve the Time as a class POSIXct). 
 
 my_coins <- lapply(X = my_coins,
        FUN = function(x){
@@ -66,5 +69,53 @@ lapply(my_coins, function(x){
   nrow(x)
 }) 
 
-# 
-                  
+# Step 03 Correlation ---- 
+
+#' Curious about both general correlation in the last 2,100+ hours 
+#' and rolling 24 hour correlations. 
+
+correlation_tbl <- data.frame(coin1 = character(),
+                              coin2 = character(),
+                              correlation = numeric())
+
+for(i in names(my_coins)){ 
+  for(j in names(my_coins)){
+    correlation_tbl <- rbind(correlation_tbl, 
+                             data.frame(coin1 = i,
+                                        coin2 = j,
+                                        correlation = 
+                                          cor(my_coins[[i]]$Market_Cap,
+                                              my_coins[[j]]$Market_Cap))
+                             ) 
+    }
+  }
+
+get_correlation_plot <- function(correlation_tbl){ 
+
+correlation_plot <- ggplot(data = correlation_tbl,
+       aes(x = coin1, y = coin2)) + 
+  geom_label(aes(label = round(correlation, 2),
+                 col = round(correlation, 2))) +
+  labs(title = "Total Market Cap Correlation",
+       sub = "June 1, 2020 - Aug 29, 2020",
+       x = "", y = "") + 
+  theme_classic() + theme(axis.text = element_text(size = 14),
+                          title = element_text(size = 14,
+                                               hjust = 0.5))
+
+correlation_plot$labels$colour <- NULL
+
+return(correlation_plot)
+} 
+get_correlation_plot(correlation_tbl)
+
+
+
+
+
+
+
+
+
+
+
